eed88064539c0fb65c1f108bc4b83a74d5c35dda
Max Horn <max.horn@bsse.ethz.ch>
2019-11-02 11:52:44 +0100

    Use black for formatting

--- tasks.py
+++ tasks.py
@@ -47,8 +47,8 @@ def format(c, check=False):
     """
     python_dirs_string = " ".join(PYTHON_DIRS)
     # Run yapf
-    yapf_options = '--recursive {}'.format('--diff' if check else '--in-place')
-    c.run("yapf {} {}".format(yapf_options, python_dirs_string))
+    black_options = '{}'.format('--diff --check' if check else '')
+    c.run("black {} {}".format(black_options, python_dirs_string))
     # Run isort
     isort_options = '--recursive {}'.format(
         '--check-only' if check else '')
